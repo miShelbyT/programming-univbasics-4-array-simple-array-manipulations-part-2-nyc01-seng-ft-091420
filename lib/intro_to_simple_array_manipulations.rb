18a55410ffd18a99b0b8ebf1ef0dbca52caaf7d1
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-24 23:31:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
-def using_concat (array1, array2)
+def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert (array, element)
+def using_insert(array, element)
   array.insert[4] = element
 end
